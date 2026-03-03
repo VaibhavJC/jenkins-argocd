daf08d11dd95c9e09be36fcea63939d9bc0f133f
VaibhavJC <97387485+VaibhavJC@users.noreply.github.com>
2026-03-03 17:11:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ function App() {
     <div className='lg'>
     //<img src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/2744/1602744-t-f24fc98823fa" alt="" />
     <video width="320" height="240" controls>
-      <source src="video_2026-03-03_15-42-28.mp4" type="video/mp4">
-      <source src="video_2026-03-03_15-42-28.mp4" type="video/ogg">
+      <source src="video_2026-03-03_15-42-28.mp4" type="video/mp4" />
+      <source src="video_2026-03-03_15-42-28.mp4" type="video/ogg" />
       Your browser does not support the video tag.
   </video>
             <h3>2023 . 2h 54m . 4 languages . <span>U/A 16+</span> </h3>
